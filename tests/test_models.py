c1947fcb50ee8139e4c89652e511ecb732470f3f
AkogareStudying <126856213+AkogareStudying@users.noreply.github.com>
2026-03-18 15:11:22 +0300

    Add unit tests for models in tests/test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestModels(unittest.TestCase):
+    def test_model_creation(self):
+        # Add test for model creation
+        pass
+
+    def test_model_methods(self):
+        # Add test for model methods
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
